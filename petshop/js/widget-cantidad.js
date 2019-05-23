5eab8dfd779769596e99a6c69514081182b3be3b
carloslvallejo <48460163+carloslvallejo@users.noreply.github.com>
2019-05-23 10:51:50 -0400

    Add files via upload

---
+++ petshop/js/widget-cantidad.js
@@ -0,0 +1,48 @@
+function sumar1(){
+    document.getElementById("widget1").stepUp();
+}
+function resta1(){
+    document.getElementById("widget1").stepDown();
+}
+function sumar2(){
+    document.getElementById("widget2").stepUp();
+}
+function resta2(){
+    document.getElementById("widget2").stepDown();
+}
+function sumar3(){
+    document.getElementById("widget3").stepUp();
+}
+function resta3(){
+    document.getElementById("widget3").stepDown();
+}
+function sumar4(){
+    document.getElementById("widget4").stepUp();
+}
+function resta4(){
+    document.getElementById("widget4").stepDown();
+}
+function sumar5(){
+    document.getElementById("widget5").stepUp();
+}
+function resta5(){
+    document.getElementById("widget5").stepDown();
+}
+function sumar6(){
+    document.getElementById("widget6").stepUp();
+}
+function resta6(){
+    document.getElementById("widget6").stepDown();
+}
+function sumar7(){
+    document.getElementById("widget7").stepUp();
+}
+function resta7(){
+    document.getElementById("widget7").stepDown();
+}
+function sumar8(){
+    document.getElementById("widget8").stepUp();
+}
+function resta8(){
+    document.getElementById("widget8").stepDown();
+}
